90711a9090982284504c74b3a1913bb7397da968
test <863335016@qq.com>
2018-09-12 09:48:35 +0800

    测试

---
+++ login/src/ccom/chen/dao/loginDao.java
@@ -0,0 +1,6 @@
+package ccom.chen.dao;
+
+public class loginDao {
+	
+
+}
---
+++ login/src/com/chen/bean/user.java
@@ -0,0 +1,24 @@
+package com.chen.bean;
+
+public class user {
+	private String name;
+	private String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "user [name=" + name + ", password=" + password + "]";
+	}
+	
+
+}
